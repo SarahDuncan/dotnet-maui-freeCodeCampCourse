aa32c39ea4c88848267fbfe85b341f383a50d17b
CBS\sduncan <sduncan@carbenefitsolutions.co.uk>
2024-05-22 21:39:04 +0100

    add repository

---
+++ Models/ContactRepository.cs
@@ -0,0 +1,33 @@
+﻿namespace Contacts.Models
+{
+    public static class ContactRepository
+    {
+        public static List<Contact> _contacts = new List<Contact>()
+        {
+            new Contact
+            {
+                Name = "John Doe",
+                Email = "johndoe@email.com"
+            },
+            new Contact
+            {
+                Name = "Jane Doe",
+                Email = "janedoe@email.com"
+            },
+            new Contact
+            {
+                Name = "Tom Hanks",
+                Email = "tomhanks@email.com"
+            },
+            new Contact
+            {
+                Name = "Taylor Swift",
+                Email = "taylorswift@email.com"
+            }
+        };
+
+        public static List<Contact> GetContacts() => _contacts;
+        public static Contact GetContactById(int id) => _contacts.FirstOrDefault(c => c.Id == id);
+
+    }
+}
--- Views/ContactsPage.xaml.cs
+++ Views/ContactsPage.xaml.cs
@@ -1,3 +1,5 @@
+using Contacts.Models;
+
 namespace Contacts.Views;
 
 public partial class ContactsPage : ContentPage
@@ -6,38 +8,10 @@ public partial class ContactsPage : ContentPage
 	{
 		InitializeComponent();
 
-        List<Contact> contacts = new List<Contact>()
-        {
-            new Contact
-            {
-                Name = "John Doe",
-                Email = "johndoe@email.com"
-            },
-            new Contact
-            {
-                Name = "Jane Doe",
-                Email = "janedoe@email.com"
-            },
-            new Contact
-            {
-                Name = "Tom Hanks",
-                Email = "tomhanks@email.com"
-            },
-            new Contact
-            {
-                Name = "Taylor Swift",
-                Email = "taylorswift@email.com"
-            }
-        };
+        var contacts = ContactRepository.GetContacts();
 
         listContacts.ItemsSource = contacts;
 	}
-
-    public class Contact
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-    }
 
     private async void listContacts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
     {
